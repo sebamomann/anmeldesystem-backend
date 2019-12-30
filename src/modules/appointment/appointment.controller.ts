b794e4db85a0c618eed561f726b325531b990866
sebamomann <github@sebamomann.de>
2019-12-30 20:33:59 +0100

    Feature: Appointment Duplicate Link error handling

--- src/modules/appointment/appointment.controller.ts
+++ src/modules/appointment/appointment.controller.ts
@@ -1,6 +1,17 @@
-import {Body, ClassSerializerInterceptor, Controller, Get, Post, Query, UseInterceptors} from '@nestjs/common';
+import {
+    Body,
+    ClassSerializerInterceptor,
+    Controller,
+    Get,
+    HttpStatus,
+    Post,
+    Query,
+    Res,
+    UseInterceptors
+} from '@nestjs/common';
 import {Appointment} from "./appointment.entity";
 import {AppointmentService} from "./appointment.service";
+import {Response} from "express";
 
 @Controller('appointment')
 export class AppointmentController {
@@ -15,8 +26,27 @@ export class AppointmentController {
     }
 
     @Post()
-    register(@Body() appointment: Appointment): Promise<Appointment> {
-        return this.appointmentService.create(appointment);
+    register(@Body() appointment: Appointment, @Res() res: Response) {
+        return this.appointmentService.create(appointment).then(tAppointment => {
+            delete tAppointment.files;
+            res.status(HttpStatus.CREATED).json(tAppointment);
+        }).catch((err) => {
+            let error = {error: {}};
+            if (err.code === 'ER_DUP_ENTRY') {
+                error.error = {
+                    columns: [
+                        {
+                            name: "link",
+                            error: "duplicate"
+                        }
+                    ]
+                };
+            } else {
+                error.error = {undefined: {message: "Some error occurred. Please try again later or contact the support"}};
+            }
+
+            res.status(HttpStatus.BAD_REQUEST).json(error);
+        });
     }
 
     @Get('get')
