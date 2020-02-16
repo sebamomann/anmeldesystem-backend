ddfaa24e9b2c4781fcfe1fff838f4c3fc1600ab2
sebamomann <github@sebamomann.de>
2020-02-16 17:19:59 +0100

    Provide file as download

--- src/modules/file/file.service.ts
+++ src/modules/file/file.service.ts
@@ -1,5 +1,18 @@
 import {Injectable} from '@nestjs/common';
+import {getRepository, Repository} from "typeorm";
+import {InjectRepository} from "@nestjs/typeorm";
+import {File} from "./file.entity";
 
 @Injectable()
 export class FileService {
+    constructor(@InjectRepository(File)
+                private fileRepository: Repository<File>) {
+    }
+
+    async findById(id: string) {
+        return await getRepository(File)
+            .createQueryBuilder("file")
+            .where("file.id = :id", {id: id})
+            .getOne();
+    }
 }
