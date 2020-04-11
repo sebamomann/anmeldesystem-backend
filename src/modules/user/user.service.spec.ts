0fd14e2358636b29127eee09de57059e5dd12936
sebamomann <github@sebamomann.de>
2020-04-11 15:19:48 +0200

    test order and renaming

--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -172,94 +172,96 @@ describe('UserService', () => {
     });
 
     describe('* overall information (by id from jwt)', () => {
-        it('should return entity if found', async () => {
-            const userToSatisfyParameter = new User();
-
-            let mainUserValue = new User();
-            mainUserValue.emailChange = [];
-            userRepositoryMock.findOne.mockReturnValue(mainUserValue);
-
-            const actual = await userService.get(userToSatisfyParameter);
-            expect(actual).toEqual(mainUserValue);
-        });
-
-        describe('* should return entity if found with valid emailChange filtering', () => {
-            it('determine by date', async () => {
+        describe('* should return entity if found', () => {
+            it('normal', async () => {
                 const userToSatisfyParameter = new User();
 
                 let mainUserValue = new User();
-
-                let mailChangeToRemove = new EmailChange();
-                mailChangeToRemove.iat = new Date(Date.now() - (30 * 60 * 60 * 1000));
-
-                let mailChangeToKeep = new EmailChange();
-                mailChangeToKeep.iat = new Date(Date.now() - (2 * 60 * 60 * 1000));
-                mailChangeToKeep.used = null;
-
-                mainUserValue.emailChange = [mailChangeToRemove, mailChangeToKeep];
+                mainUserValue.emailChange = [];
                 userRepositoryMock.findOne.mockReturnValue(mainUserValue);
 
                 const actual = await userService.get(userToSatisfyParameter);
                 expect(actual).toEqual(mainUserValue);
-                expect(actual.emailChange).toEqual([mailChangeToKeep]);
             });
 
-            it('determine by used state', async () => {
-                const userToSatisfyParameter = new User();
+            describe('* with valid emailChange filtering', () => {
+                it('determine by date', async () => {
+                    const userToSatisfyParameter = new User();
 
-                let mainUserValue = new User();
+                    let mainUserValue = new User();
 
-                let mailChangeToRemove = new EmailChange();
-                mailChangeToRemove.iat = new Date(Date.now() - (2 * 60 * 60 * 1000));
-                mailChangeToRemove.used = new Date();
+                    let mailChangeToRemove = new EmailChange();
+                    mailChangeToRemove.iat = new Date(Date.now() - (30 * 60 * 60 * 1000));
 
-                let mailChangeToKeep = new EmailChange();
-                mailChangeToKeep.iat = new Date(Date.now() - (2 * 60 * 60 * 1000));
-                mailChangeToKeep.used = null;
+                    let mailChangeToKeep = new EmailChange();
+                    mailChangeToKeep.iat = new Date(Date.now() - (2 * 60 * 60 * 1000));
+                    mailChangeToKeep.used = null;
 
-                mainUserValue.emailChange = [mailChangeToRemove, mailChangeToKeep];
-                userRepositoryMock.findOne.mockReturnValue(mainUserValue);
+                    mainUserValue.emailChange = [mailChangeToRemove, mailChangeToKeep];
+                    userRepositoryMock.findOne.mockReturnValue(mainUserValue);
 
-                const actual = await userService.get(userToSatisfyParameter);
-                expect(actual).toEqual(mainUserValue);
-                expect(actual.emailChange).toEqual([mailChangeToKeep]);
-            });
-
-            it('remove emailChange list if empty', async () => {
-                const userToSatisfyParameter = new User();
-
-                let mainUserValue = new User();
-
-                let mailChangeToRemove = new EmailChange();
-                mailChangeToRemove.iat = new Date(Date.now() - (2 * 60 * 60 * 1000));
-                mailChangeToRemove.used = new Date();
-
-                let mailChangeToRemove2 = new EmailChange();
-                mailChangeToRemove2.iat = new Date(Date.now() - (2 * 60 * 60 * 1000));
-                mailChangeToRemove2.used = new Date();
-
-                mainUserValue.emailChange = [mailChangeToRemove, mailChangeToRemove2];
-                userRepositoryMock.findOne.mockReturnValue(mainUserValue);
-
-                const actual = await userService.get(userToSatisfyParameter);
-                expect(actual).toEqual(mainUserValue);
-                expect(actual.emailChange).toEqual(undefined);
-            });
-        });
-
-        it('should return error if entity not found', async () => {
-            const userToSatisfyParameter = new User();
-
-            const user = undefined;
-
-            userRepositoryMock.findOne.mockReturnValue(user);
-            userService.get(userToSatisfyParameter)
-                .then(() => {
-                    throw new Error('I have failed you, Anakin. Should have gotten an EntityNotFoundException');
-                })
-                .catch((err) => {
-                    expect(err).toBeInstanceOf(EntityNotFoundException);
+                    const actual = await userService.get(userToSatisfyParameter);
+                    expect(actual).toEqual(mainUserValue);
+                    expect(actual.emailChange).toEqual([mailChangeToKeep]);
                 });
+
+                it('determine by used state', async () => {
+                    const userToSatisfyParameter = new User();
+
+                    let mainUserValue = new User();
+
+                    let mailChangeToRemove = new EmailChange();
+                    mailChangeToRemove.iat = new Date(Date.now() - (2 * 60 * 60 * 1000));
+                    mailChangeToRemove.used = new Date();
+
+                    let mailChangeToKeep = new EmailChange();
+                    mailChangeToKeep.iat = new Date(Date.now() - (2 * 60 * 60 * 1000));
+                    mailChangeToKeep.used = null;
+
+                    mainUserValue.emailChange = [mailChangeToRemove, mailChangeToKeep];
+                    userRepositoryMock.findOne.mockReturnValue(mainUserValue);
+
+                    const actual = await userService.get(userToSatisfyParameter);
+                    expect(actual).toEqual(mainUserValue);
+                    expect(actual.emailChange).toEqual([mailChangeToKeep]);
+                });
+
+                it('remove emailChange list if empty', async () => {
+                    const userToSatisfyParameter = new User();
+
+                    let mainUserValue = new User();
+
+                    let mailChangeToRemove = new EmailChange();
+                    mailChangeToRemove.iat = new Date(Date.now() - (2 * 60 * 60 * 1000));
+                    mailChangeToRemove.used = new Date();
+
+                    let mailChangeToRemove2 = new EmailChange();
+                    mailChangeToRemove2.iat = new Date(Date.now() - (2 * 60 * 60 * 1000));
+                    mailChangeToRemove2.used = new Date();
+
+                    mainUserValue.emailChange = [mailChangeToRemove, mailChangeToRemove2];
+                    userRepositoryMock.findOne.mockReturnValue(mainUserValue);
+
+                    const actual = await userService.get(userToSatisfyParameter);
+                    expect(actual).toEqual(mainUserValue);
+                    expect(actual.emailChange).toEqual(undefined);
+                });
+            });
+
+            it('should return error if entity not found', async () => {
+                const userToSatisfyParameter = new User();
+
+                const user = undefined;
+
+                userRepositoryMock.findOne.mockReturnValue(user);
+                userService.get(userToSatisfyParameter)
+                    .then(() => {
+                        throw new Error('I have failed you, Anakin. Should have gotten an EntityNotFoundException');
+                    })
+                    .catch((err) => {
+                        expect(err).toBeInstanceOf(EntityNotFoundException);
+                    });
+            });
         });
     });
 
@@ -643,7 +645,7 @@ describe('UserService', () => {
                         });
                 });
 
-                it('should return error if failed (just forward thrown error [InvalidTokenException])', async () => {
+                it('InvalidTokenException', async () => {
                     const mail = 'mail@example.com';
                     const token = 'token';
                     const password = 'password';
