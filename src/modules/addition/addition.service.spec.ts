d8292be8861b72a051197a1b77e5f755632dc30e
sebamomann <github@sebamomann.de>
2020-07-29 21:02:21 +0200

    100% addition test coverage
    Signed-off-by: sebamomann <github@sebamomann.de>

--- src/modules/addition/addition.service.spec.ts
+++ src/modules/addition/addition.service.spec.ts
@@ -5,6 +5,7 @@ import {Repository} from 'typeorm';
 import {MockType, repositoryMockFactory} from '../appointment/appointment.service.spec';
 import {getRepositoryToken} from '@nestjs/typeorm';
 import {Addition} from './addition.entity';
+import {EntityNotFoundException} from '../../exceptions/EntityNotFoundException';
 
 describe('AdditionService', () => {
     let additionService: AdditionService;
@@ -29,21 +30,77 @@ describe('AdditionService', () => {
         expect(additionService).toBeDefined();
     });
 
-    describe('get by name and appointment', () => {
-        it('should return entity if successful', async () => {
-            const name = 'addition1';
+    describe('* get by id', () => {
+        it('* successful should return entity', async () => {
+            const __given_id = '5faccdfa-62b8-4cc5-81bd-c001e5804dd7';
 
-            const appointment = new Appointment();
-            appointment.id = '1';
+            const __existing_addition = new Addition();
+            __existing_addition.id = __given_id;
+            __existing_addition.name = 'addition';
 
-            const addition = new Addition();
-            addition.name = name;
-            addition.appointment = appointment;
+            additionRepositoryMock.findOne.mockReturnValueOnce(__existing_addition);
 
-            additionRepositoryMock.findOne.mockReturnValueOnce(addition);
+            const __expected = __existing_addition;
 
-            const actual = await additionService.findByNameAndAppointment(name, appointment);
-            expect(actual).toEqual(addition);
+            const __actual = await additionService.findById(__given_id);
+            expect(__actual).toEqual(__expected);
+        });
+
+        describe('* failure should return error', () => {
+            it('* addition not found', async (done) => {
+                const __given_id = '5faccdfa-62b8-4cc5-81bd-c001e5804dd7';
+
+                additionRepositoryMock.findOne.mockReturnValueOnce(undefined);
+
+                try {
+                    await additionService.findById(__given_id);
+                    done.fail(new Error('I have failed you, Anakin. Should have gotten an EntityNotFoundException'));
+                } catch (e) {
+                    expect(e).toBeInstanceOf(EntityNotFoundException);
+                    expect(e.data).toEqual('addition');
+                    done();
+                }
+            });
+        });
+    });
+
+    describe('* get by name and appointment', () => {
+        it('* successful should return entity', async () => {
+            const __given_name = 'addition1';
+
+            const __existing_appointment = new Appointment();
+            __existing_appointment.id = '1';
+
+            const __existing_addition = new Addition();
+            __existing_addition.id = '5faccdfa-62b8-4cc5-81bd-c001e5804dd7';
+            __existing_addition.name = __given_name;
+            __existing_addition.appointment = __existing_appointment;
+
+            additionRepositoryMock.findOne.mockReturnValueOnce(__existing_addition);
+
+            const __expected = __existing_addition;
+
+            const __actual = await additionService.findByNameAndAppointment(__given_name, __existing_appointment);
+            expect(__actual).toEqual(__expected);
+        });
+
+        describe('* failure should return error', () => {
+            it('* addition not found', async (done) => {
+                const __given_name = 'addition1';
+
+                const __existing_appointment = new Appointment();
+
+                additionRepositoryMock.findOne.mockReturnValueOnce(undefined);
+
+                try {
+                    await additionService.findByNameAndAppointment(__given_name, __existing_appointment);
+                    done.fail(new Error('I have failed you, Anakin. Should have gotten an EntityNotFoundException'));
+                } catch (e) {
+                    expect(e).toBeInstanceOf(EntityNotFoundException);
+                    expect(e.data).toEqual('addition');
+                    done();
+                }
+            });
         });
     });
 });
--- src/modules/user/user.mapper.ts
+++ src/modules/user/user.mapper.ts
@@ -5,8 +5,24 @@ const emailChangeMapper = require('./email-change/email-change.mapper');
 
 module.exports = {
     basic: function(userService: UserService, _user: User) {
-        const newUser = (({id, name, username, mail, emailChange, iat, refreshToken}) =>
-            ({id, name, username, mail, emailChange: userService.retrieveActiveMailChanges(emailChange), iat, refreshToken}))
+        const newUser = (({
+                              id,
+                              name,
+                              username,
+                              mail,
+                              emailChange,
+                              iat,
+                              refreshToken
+                          }) =>
+            ({
+                id,
+                name,
+                username,
+                mail,
+                emailChange: userService.retrieveActiveMailChanges(emailChange),
+                iat,
+                refreshToken,
+            }))
         (_user);
 
         if (newUser.emailChange.length === 0) {
