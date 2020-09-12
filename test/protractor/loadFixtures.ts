1174cc45a73a97f62f0c3c3c6576f4c1ea3db537
sebamomann <github@sebamomann.de>
2020-09-12 14:26:08 +0200

    disable foreign key check on database clearance
    Signed-off-by: sebamomann <github@sebamomann.de>

--- test/protractor/loadFixtures.ts
+++ test/protractor/loadFixtures.ts
@@ -7,11 +7,12 @@ export async function loadFixtures(connection: Connection): Promise<any> {
 
     // CLEAR DATABASE
     const entities = connection.entityMetadatas;
-
+    await connection.query('SET FOREIGN_KEY_CHECKS=0;');
     for (const entity of entities) {
         const repository = await connection.getRepository(entity.name);
         await repository.query(`DELETE FROM ${entity.tableName};`);
     }
+    await connection.query('SET FOREIGN_KEY_CHECKS=1;');
 
     setTimeout(() => {
 
