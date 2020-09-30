e4f1ad1e183fa5f50f6b93f2b8b5c66e8d1fadcd
sebamomann <github@sebamomann.de>
2020-09-30 13:28:15 +0200

    dont log to file
    Signed-off-by: sebamomann <github@sebamomann.de>

--- src/logger.ts
+++ src/logger.ts
@@ -2,36 +2,15 @@ const winston = require('winston');
 
 const fs = require('fs');
 
-fs.mkdirSync('/logs');
-
 let logger;
 
-if (process.env.NODE_ENV === 'production') {
-    logger = winston.createLogger({
-        level: 'info',
-        format: winston.format.json(),
-        defaultMeta: {service: 'user-service'},
-        transports: [
-            new winston.transports.File({filename: '/logs/error.log', level: 'error'}),
-            new winston.transports.File({filename: '/logs/combined.log'})
-        ]
-    });
-} else {
-    logger = winston.createLogger({
-        level: 'info',
-        format: winston.format.json(),
-        defaultMeta: {service: 'user-service'},
-        transports: [
-            new winston.transports.Console()
-        ]
-    });
-}
-
-/* istanbul ignore next */
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-    }));
-}
+logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: {service: 'user-service'},
+    transports: [
+        new winston.transports.Console()
+    ]
+});
 
 module.exports = logger;
