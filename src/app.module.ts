74179ab6641662c66ea14ab2971ed4584ab17fee
sebamomann <github@sebamomann.de>
2021-03-12 21:15:10 +0100

    remove unusedvariable
    Signed-off-by: sebamomann <github@sebamomann.de>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
-import {Connection} from 'typeorm';
 import {UserModule} from './modules/user/user.module';
 import {AppointmentModule} from './modules/appointment/appointment.module';
 import {EnrollmentModule} from './modules/enrollment/enrollment.module';
@@ -53,6 +52,6 @@ const _mail = process.env.MAIL_ECA;
     providers: [AppService],
 })
 export class AppModule {
-    constructor(private readonly connection: Connection) {
+    constructor() {
     }
 }
